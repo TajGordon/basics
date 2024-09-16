c2601ae52bc9e99c2f86293cccbfd7830f03bc1f
TajGordon <tajgordon@gmail.com>
2024-09-16 21:40:07 +1000

    aaa

---
+++ iter_2/buildforwin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cflags="-std=c++20 -Wall PLATFORM=PLATFORM_DESKTOP"
+includes="-I/opt/homebrew/Cellar/raylib/5.0/include"
+libs="-lraylib -L/opt/homebrew/Cellar/openssl@3/3.3.2/lib"
+
+output="-ogame"
+
+c++ $includes src/main.cpp $output $libs $cflags $warnings
